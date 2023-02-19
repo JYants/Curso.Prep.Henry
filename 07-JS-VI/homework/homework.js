bbcad14a1600e5fbf62af3f6a545a77329b81ee6
Juan Yotti <yottijuan2002@gmail.com>
2023-02-18 22:58:18 -0300

    JSVI 100% completo

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -25,23 +25,29 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
- function sumaNumeros(cb){
-  
- }
+  let sumaReduce = numeros.reduce(function(acc, element){
+    return acc + element;
+  });
+  cb(sumaReduce);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-}
+  array.forEach(function(element){
+    cb(element);
+  });
+} 
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  let newArray = array.map(cb => cb);
+  let newArray = array.map(function(element){
+    return cb(element);
+  });
   return newArray;
 }
 
